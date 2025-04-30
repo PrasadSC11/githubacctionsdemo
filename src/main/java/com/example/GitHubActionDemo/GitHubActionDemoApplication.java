c94df85a220b6a0f381519c17a93238df6c4835d
PrasadSC11 <prasadchinchkar11@gmail.com>
2025-04-30 14:07:48 +0530

    first

---
+++ src/main/java/com/example/GitHubActionDemo/GitHubActionDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.GitHubActionDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitHubActionDemoApplication {
+
+	@GetMapping("/")
+	public String getString(){
+		return "welcome";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionDemoApplication.class, args);
+	}
+
+}
